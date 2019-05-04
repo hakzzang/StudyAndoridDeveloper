1fd19cdd85965a2a3e8884d7fee455db1801c91b
byeonghak-moon <byeonghak.moon@navercorp.com>
2019-05-04 19:30:51 +0900

    update observer pattern example.

--- src/observer_pattern/ObserverPattern.java
+++ src/observer_pattern/ObserverPattern.java
@@ -3,22 +3,33 @@ package observer_pattern;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+//2019.05.xx 19:11...
+//observer pattern은 유용할 것 같다.
+//Subject는 Observer 객체에게 변화를 제공해준다.
+//Observer 객체는 변화된 객체를 가공한다.
+//LiveData 또한 Observer pattern의 대표적인 예다.
 public class ObserverPattern {
 	public static void main(String[] args) {
 		CCTV cctv = CCTV.getInstance();
 		CCTVManager cctvManager = new CCTVManager(1);
 		CCTVManager cctvManager2 = new CCTVManager(2);
-		CCTVManager cctvManager3 = new CCTVManager(3);
+		CCTVManager cctvManager3 = new CCTVManager(3);//3번은 감시를 안하고 있는 걸 알 수 있음.
+
+		
 		cctv.registerObserver(cctvManager);
 		cctv.registerObserver(cctvManager2);
+		
 		cctv.watchPeople("MoonByeongHak", "Coding");
 		cctv.watchPeople("MoonByeongHak", "coffee time");
-		cctv.removeObserver(cctvManager2);
+		
+		cctv.removeObserver(cctvManager2);//2번이 감를 안 함.
+		
 		cctv.watchPeople("Shirimp burger", "foo~~");
 	}
 }
 
 class CCTV implements Subject{
+	//싱글톤 객체로 유일한 Subject를 만듦.
 	public static CCTV cctvInstance = new CCTV();
 	private ArrayList<Observer> cctvObserverArray = new ArrayList();
 	private String peopleName;
@@ -63,7 +74,8 @@ class CCTV implements Subject{
 	}
 }
 
-class CCTVManager implements ObserverIterator , Observer{
+//CCTVManager==Observer,는 데이터가 들어오면 행위를 하는 존재.
+class CCTVManager implements DisplayInterface , Observer{
 	private int cctvIndex;
 	private String peopleName;
 	private String behavior;
@@ -85,9 +97,7 @@ class CCTVManager implements ObserverIterator , Observer{
 	}
 	
 }
-interface ObserverIterator{
-	void display();
-}
+
 
 interface Subject{
 	void registerObserver(Observer observer);
