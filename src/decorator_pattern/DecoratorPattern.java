6d0f8953e24b7e0e83c9044bb445ab872ce8e6e0
byeonghak-moon <byeonghak.moon@navercorp.com>
2019-05-04 20:55:00 +0900

    Add DecoratorPattern Example.

---
+++ src/decorator_pattern/DecoratorPattern.java
@@ -0,0 +1,89 @@
+package decorator_pattern;
+
+public class DecoratorPattern {
+	public static void main(String[] args) {
+		Drill superDrill = new SuperDrill();
+		System.out.println(superDrill.getItemName());
+		System.out.println("Power:"+superDrill.power());
+		superDrill = new Ruby(superDrill);
+		System.out.println(superDrill.getItemName());
+		System.out.println("Power:"+superDrill.power());
+		superDrill =new Gold(superDrill);
+		System.out.println(superDrill.getItemName());
+		System.out.println("Power:"+superDrill.power());
+	}
+}
+
+class SuperDrill extends Drill{
+	SuperDrill(){
+		itemName ="Super Drill::";
+	}
+	
+	@Override
+	public double power() {
+		return 100;
+	}
+}
+
+class BrokenDrill extends Drill{
+	BrokenDrill(){
+		itemName = "Broken Drill::";
+	}
+
+	@Override
+	public double power() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+}
+
+abstract class Drill{
+	String itemName;
+	
+	public String getItemName() {
+		return itemName;
+	}
+	
+	public abstract double power();
+}
+
+class Ruby extends Enchant{
+	Drill drill;
+	Ruby(Drill drill){
+		this.drill = drill;
+	}
+	
+	@Override
+	public String getItemName() {
+		// TODO Auto-generated method stub
+		return drill.getItemName() +"루비::";
+	}
+
+	@Override
+	public double power() {
+		// TODO Auto-generated method stub
+		return drill.power() + 200.0 ;
+	}
+	
+}
+
+class Gold extends Enchant{
+	Drill drill;
+	Gold(Drill drill){
+		this.drill = drill;
+	}
+	@Override
+	public String getItemName() {
+		// TODO Auto-generated method stub
+		return drill.getItemName() +"골드::";
+	}
+	@Override
+	public double power() {
+		// TODO Auto-generated method stub
+		return drill.power() + 1000.0;
+	}
+}
+
+abstract class Enchant extends Drill{
+	public abstract String getItemName();
+}
